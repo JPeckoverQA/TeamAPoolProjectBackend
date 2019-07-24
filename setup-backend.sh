632ffde759b3a6e95a37d4cd10c06c83121d087d
JPeckoverQA <50712067+JPeckoverQA@users.noreply.github.com>
2019-07-24 13:08:37 +0100

    update docker repo path

--- setup-backend.sh
+++ setup-backend.sh
@@ -5,7 +5,7 @@
 #docker run -d --name mongo --network pool_app_network mongo:latest
 
 #docker build -t docker.io/jpeckover/pool_backend:latest
-docker pull jpeckover/pool_backend:latest
+docker pull jpeckover/pool-backend-aws:latest
 
 docker run -d --name backend -e MONGO_HOST=10.0.0.34 -p 8080:8080 jpeckover/pool-backend-aws:latest
 
