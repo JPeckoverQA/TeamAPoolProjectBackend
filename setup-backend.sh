d0527beacf64f4f261a6c058e5f86dd98a6b33b7
JPeckoverQA <50712067+JPeckoverQA@users.noreply.github.com>
2019-07-24 11:14:53 +0100

    typo // -> #

--- setup-backend.sh
+++ setup-backend.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-//docker network create pool_app_network
+#docker network create pool_app_network
 
-//docker run -d --name mongo --network pool_app_network mongo:latest
+#docker run -d --name mongo --network pool_app_network mongo:latest
 
-//docker build -t docker.io/jpeckover/pool_backend:latest
+#docker build -t docker.io/jpeckover/pool_backend:latest
 docker pull jpeckover/pool_backend:latest
 
 docker run -d --name backend --network pool_app_network -p 8080:8080 /jpeckover/pool_backend:latest
