fa3da3788582ed2cb3fc30b01581de15b299b28b
JPeckoverQA <50712067+JPeckoverQA@users.noreply.github.com>
2019-07-24 11:34:32 +0100

    remove network for multiple vms

--- setup-backend.sh
+++ setup-backend.sh
@@ -7,7 +7,7 @@
 #docker build -t docker.io/jpeckover/pool_backend:latest
 docker pull jpeckover/pool_backend:latest
 
-docker run -d --name backend --network pool_app_network -e MONGO_HOST=ip-10-0-0-34.eu-west-2.compute.internal -p 8080:8080 jpeckover/pool_backend:latest
+docker run -d --name backend -e MONGO_HOST=ip-10-0-0-34.eu-west-2.compute.internal -p 8080:8080 jpeckover/pool_backend:latest
 
 #docker ps
 #docker logs -f backend
