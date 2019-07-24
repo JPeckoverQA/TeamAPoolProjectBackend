c54cd7b0b7bbacefbf937cbc62b22d7706fa1b62
JPeckoverQA <50712067+JPeckoverQA@users.noreply.github.com>
2019-07-24 13:07:04 +0100

    update docker repo name

--- setup-backend.sh
+++ setup-backend.sh
@@ -7,7 +7,7 @@
 #docker build -t docker.io/jpeckover/pool_backend:latest
 docker pull jpeckover/pool_backend:latest
 
-docker run -d --name backend -e MONGO_HOST=10.0.0.34 -p 8080:8080 jpeckover/pool_backend:latest
+docker run -d --name backend -e MONGO_HOST=10.0.0.34 -p 8080:8080 jpeckover/pool-backend-aws:latest
 
 #docker ps
 #docker logs -f backend
