809349727b5c8aca518c86946a24b8c11274c000
JPeckoverQA <50712067+JPeckoverQA@users.noreply.github.com>
2019-07-25 13:44:26 +0100

    added sudo

--- setup-backend.sh
+++ setup-backend.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-docker network create pool_app_network
+sudo docker network create pool_app_network
 
-docker run -d --name mongo --network pool_app_network mongo:latest
+sudo docker run -d --name mongo --network pool_app_network mongo:latest
 
-//docker build -t docker.io/jpeckover/pool_backend:latest .
-docker pull jpeckover/pool_backend:latest .
+#docker build -t docker.io/jpeckover/pool_backend:latest .
+sudo docker pull jpeckover/pool_backend:latest .
 
-docker run -d --name backend --network pool_app_network -p 8080:8080 /jpeckover/pool_backend:latest
+sudo docker run -d --name backend --network pool_app_network -p 8080:8080 /jpeckover/pool_backend:latest
 
 #docker ps
 #docker logs -f backend
