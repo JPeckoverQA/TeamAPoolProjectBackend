b394d1d54636722f9d5b7eb66507af2bfad48f1c
JPeckoverQA <50712067+JPeckoverQA@users.noreply.github.com>
2019-07-25 13:59:25 +0100

    remove .

--- setup-backend.sh
+++ setup-backend.sh
@@ -5,7 +5,7 @@ sudo docker network create pool_app_network
 sudo docker run -d --name mongo --network pool_app_network mongo:latest
 
 #docker build -t docker.io/jpeckover/pool_backend:latest .
-sudo docker pull jpeckover/pool_backend:latest .
+sudo docker pull jpeckover/pool_backend:latest
 
 sudo docker run -d --name backend --network pool_app_network -p 8080:8080 /jpeckover/pool_backend:latest
 
