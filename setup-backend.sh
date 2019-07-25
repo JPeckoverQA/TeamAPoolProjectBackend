ae1b7b90c63892f8a4c5f60644487545543ffddb
JPeckoverQA <50712067+JPeckoverQA@users.noreply.github.com>
2019-07-25 16:27:54 +0100

    build instead of run

--- setup-backend.sh
+++ setup-backend.sh
@@ -4,8 +4,8 @@
 
 #docker run -d --name mongo --network pool_app_network mongo:latest
 
-#docker build -t docker.io/jpeckover/pool_backend:latest
-sudo docker pull jpeckover/pool-backend-aws:latest
+sudo docker build -t docker.io/jpeckover/pool_backend:latest
+#sudo docker pull jpeckover/pool-backend-aws:latest
 
 sudo docker run -d --name backend -e MONGO_HOST=${aws_instance.mongo.private_ip} -p 8080:8080 jpeckover/pool-backend-aws:latest
 
