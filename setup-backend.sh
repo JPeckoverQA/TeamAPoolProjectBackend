197cb9b82e7f3927295ac1194beb0ba63ed10133
JPeckoverQA <50712067+JPeckoverQA@users.noreply.github.com>
2019-07-25 17:37:22 +0100

    correct docker repo

--- setup-backend.sh
+++ setup-backend.sh
@@ -5,9 +5,9 @@
 #docker run -d --name mongo --network pool_app_network mongo:latest
 
 #sudo docker build -t docker.io/jpeckover/pool_backend:latest
-sudo docker pull jpeckover/pool-backend-aws:latest
+sudo docker pull jpeckover/pool-backend-aws-terraform:latest
 
-sudo docker run -d --name backend -e MONGO_HOST=${aws_instance.mongo.private_ip} -p 8080:8080 jpeckover/pool-backend-aws:latest
+sudo docker run -d --name backend -e MONGO_HOST=${aws_instance.mongo.private_ip} -p 8080:8080 jpeckover/pool-backend-aws-terraform:latest
 
 #docker ps
 #docker logs -f backend
