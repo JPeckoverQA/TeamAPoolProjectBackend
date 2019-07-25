51c857850a5a14b5673bc58beb3e3120f5a1e186
JPeckoverQA <50712067+JPeckoverQA@users.noreply.github.com>
2019-07-25 16:02:55 +0100

    typo

--- setup-backend.sh
+++ setup-backend.sh
@@ -7,7 +7,7 @@
 #docker build -t docker.io/jpeckover/pool_backend:latest
 sudo docker pull jpeckover/pool-backend-aws:latest
 
-sudo docker run -d --name backend -e MONGO_HOST=${aws_instance_mongo.private_ip} -p 8080:8080 jpeckover/pool-backend-aws:latest
+sudo docker run -d --name backend -e MONGO_HOST=${aws_instance.mongo.private_ip} -p 8080:8080 jpeckover/pool-backend-aws:latest
 
 #docker ps
 #docker logs -f backend
